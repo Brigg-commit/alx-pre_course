fd55934481a844253a1e6471162e20871faff93a
Briggs Jimt <ibridget95@gmail.com>
2022-06-17 08:25:49 +0100

    Delete 0x01-git directory

--- 0x01-git/touch c_is_fun.c
+++
@@ -1,7 +0,0 @@
-Create this empty files
-c/c_is_fun.c
-js/main.js
-js/index.js
-Add to git
-Commit -m “Starting to code today, so cool” 
-Push to the remote server.
--- 0x01-git/touch main.js
+++
@@ -1,9 +0,0 @@
-Create these empty files
-c/c_is_fun.c
-js/main.js
-js/index.js
-Add to git
-Commit -m “Starting to code today, so cool” 
-Push to the remote server.
-
-
